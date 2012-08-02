fdeea7d73e9f58f9528fa3e08ef88b5c03f25df9
MPogoda <do0dlez1991@gmail.com>
2012-08-02 22:05:25 +0300

    implementing these interfaces

---
+++ CurrentConditionsDisplay.java
@@ -0,0 +1,24 @@
+package ua.patterns;
+
+public class CurrentConditionsDisplay implements Observer {
+  private float temperature;
+  private float humidity;
+  private Subject weatherData;
+
+  public CurrentConditionsDisplay(Subject weatherData) {
+    this.weatherData = weatherData();
+    weatherData.registerObserver(this);
+  }
+
+  public void update(float temperature, float humidity, float _) {
+    this.temperature = temperature;
+    this.humidity    = humidity;
+
+    display();
+  }
+
+  public void display() {
+    System.out.println("Current conditions : " + temperature + "°C and " +
+        humidity + " % humidity.");
+  }
+}
--- WeatherData.java
+++ WeatherData.java
@@ -1,9 +1,34 @@
 package ua.patterns;
 
-public final class WeatherData {
+public final class WeatherData implements Subject {
+  private ArrayList<Observer> observers;
   private float temperature;
   private float humidity;
   private float pressure;
+
+  public WeatherData() {
+    observers = new ArrayList<Observer>();
+  }
+
+  @Override
+  public void registerObserver(Observer observer) {
+    observers.add(observer);
+  }
+
+  @Override
+  public void removeObserver(Observer observer) {
+    int i = observers.indexOf(observer);
+    if (0 <= i) {
+      observers.remove(i);
+    }
+  }
+
+  @Override
+  public void notifyObservers() {
+    for (Observer observer : observers) {
+      observer.update(temperature, humidity, pressure);
+    }
+  }
 
   public float getTemperature() {
     return temperature;
@@ -18,6 +43,14 @@ public final class WeatherData {
   }
 
   public void measurementsChanged() {
-    // write your code here
+    notifyObservers();
+  }
+
+  public void setMeasurements(float temperature, float humidity, float pressure) {
+    this.temperature = temperature;
+    this.humidity    = humidity;
+    this.pressure    = pressure;
+
+    measurementsChanged();
   }
 }
